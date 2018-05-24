63271b8ae7b3fada8f67e984db7522a0488cbc30
wuwenfei <wwf_work@163.com>
2018-05-24 08:58:21 +0800

    对vue-cli生成的文件做了一些注释

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -5,60 +5,77 @@ const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 var webpack=require('webpack')
 
+//获取绝对路径 path.join连接路径 _dirname 获取当前文件的绝对路径
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-
-
 module.exports = {
   context: path.resolve(__dirname, '../'),
+  // webpack入口文件
   entry: {
     app: './src/main.js'
   },
+  // webpack输出路径和命名规则
   output: {
+    // webpack输出的目标文件夹路径
     path: config.build.assetsRoot,
+    // webpack输出bundle文件命名格式
     filename: '[name].js',
+    // webpack编译输出的发布路径
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+  // 模块resolve的规则
   resolve: {
     extensions: ['.js', '.vue', '.json'],
+    // 别名，方便引用模块，例如有了别名后
+    // import Vue from 'vue/dis/vue/vue.common.js‘ 可以写成 import Vue from 'vue'
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
     }
   },
+  // 不同类型的模块的处理规则
   module: {
     rules: [
+      // 对所有.vue文件使用vue-loader进行编译
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
       },
+      // 对src/和test文件夹下的.js文件使用babel-loader将es6代码转化成es5
       {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
+      // 对图片资源使用 url-loader
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
+          // 小于10k的图片转成base64编码的dataURL字符串写到代码中
           limit: 10000,
+          // 其他图片转移到静态资源文件夹
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
+      // 对多媒体文件使用url-loader
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
+          // 小于10k的资源转换成base64编码的dataURL字符串写到代码中
           limit: 10000,
+          // 其他资源转移到静态资源文件夹
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
       {
+        // 对字体文件使用url-loader
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
